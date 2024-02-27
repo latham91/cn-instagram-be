eb5a12d7c64b3bb71fc03be006a298de607c3ab9
Aaron Latham <latham91@icloud.com>
2024-02-27 20:42:53 +0000

    enhancement: added email validation

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { isEmail } from "validator";
 
 const userSchema = new mongoose.Schema(
     {
@@ -13,6 +14,7 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true,
             unique: true,
+            validate: [isEmail, "Please enter a valid email"],
         },
         password: {
             type: String,
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createPost, getPosts } from "../controllers/postController.js";
+import { createPost, deletePost, getPosts, getUserPosts } from "../controllers/postController.js";
 import verifyJwt from "../middleware/verifyJwt.js";
 
 const router = Router();
 
 router.get("/", getPosts);
 router.post("/create", verifyJwt, createPost);
+router.delete("/:postId", verifyJwt, deletePost);
+router.get("/:username", getUserPosts);
 
 export default router;
