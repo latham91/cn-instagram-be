a0c85323a3cd2091851a73e05e9e04c8bee4eca0
Aaron Latham <latham91@icloud.com>
2024-02-28 14:23:12 +0000

    update verify

--- src/middleware/verifyJwt.js
+++ src/middleware/verifyJwt.js
@@ -1,10 +1,9 @@
 import jwt from "jsonwebtoken";
-import User from "../models/userModel.js";
 
 const verifyJwt = async (req, res, next) => {
     const token = req.cookies.insta_auth;
 
-    if (!token) {
+    if (!token || token === "") {
         res.clearCookie("insta_auth");
         return res.status(401).json({ success: false, source: "verifyJwt", message: "Unauthorized" });
     }
