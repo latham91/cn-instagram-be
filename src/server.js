6728433888d6d4b04deea706e722e5657647d99b
Aaron Latham <latham91@icloud.com>
2024-02-28 13:32:57 +0000

    chore: updated cors

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
     cors({
-        origin: ["http://localhost:5173", "http://192.168.1.145:5173", "https://cn-instagram-bes.onrender.com"],
+        origin: ["http://localhost:5173", "http://192.168.1.145:5173", "https://gramster.netlify.app"],
         credentials: true,
     })
 );
