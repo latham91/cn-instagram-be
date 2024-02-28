ef7ce990fe869118e143c494ae6d0ad3f23bc182
Aaron Latham <latham91@icloud.com>
2024-02-28 15:00:41 +0000

    update verifyJwt

--- src/middleware/verifyJwt.js
+++ src/middleware/verifyJwt.js
@@ -1,20 +1,18 @@
 import jwt from "jsonwebtoken";
 
 const verifyJwt = async (req, res, next) => {
-    const token = req.cookies.insta_auth;
-
-    if (!token || token === "") {
-        await res.clearCookie("insta_auth");
-        return res.status(401).json({ success: false, source: "verifyJwt", message: "Unauthorized" });
-    }
-
     try {
+        const token = req.cookies.insta_auth;
+
+        if (!token) {
+            return res.status(401).json({ success: false, source: "verifyJwt", message: "Unauthorized" });
+        }
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
 
         next();
     } catch (error) {
-        await res.clearCookie("insta_auth");
         return res.status(401).json({ success: false, message: "Not authorized to access this route" });
     }
 };
