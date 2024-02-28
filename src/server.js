2366999b91b0cc9080802af521171b80256efd38
Aaron Latham <latham91@icloud.com>
2024-02-28 13:28:46 +0000

    chore: updated cors paths

--- src/server.js
+++ src/server.js
@@ -21,7 +21,12 @@ dotenv.config();
 const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:5173", "http://192.168.1.145:5173"], credentials: true }));
+app.use(
+    cors({
+        origin: ["http://localhost:5173", "http://192.168.1.145:5173", "https://cn-instagram-bes.onrender.com"],
+        credentials: true,
+    })
+);
 
 // Dev logging
 if (process.env.NODE_ENV === "development") {
